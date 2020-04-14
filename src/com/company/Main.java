a3ac090bedd3e67f7dab53dbbd9c2a26aa9dc2ae
Beni Paval <beni.paval@gmail.com>
2020-04-14 16:45:29 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+
+        System.out.println("Beni");
+
+        int x= 7;
+        int y= 8;
+        int z= x + y;
+        System.out.println("The addition of 7 and 8 is: " + z);
+
+        float w = 10 /3f;
+        System.out.println(w);
+
+       int a = -5 + 8 * 6;
+       System.out.println("Rezolvarea de la punctul a este: " + a);
+
+       int b =  (55+9) % 9;
+       System.out.println("Rezolvarea de la punctul b este: " + b);
+
+       int c = 20 + -3*5 / 8;
+       System.out.println("Rezolvarea de la punctul c este: " + c);
+
+       int d = 5 + 15 / 3 * 2 - 8 % 3;
+       System.out.println("Rezolvarea de la punctul d este: " + d);
+    }
+}
